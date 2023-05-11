c1cefb8ade1f3cea1403074eae09cffa3cfbaaf2
Pablo Sánchez <pablosanchez19@estudiantec.cr>
2023-05-11 02:36:55 -0600

    Finalizaciond de la clase StringLinkedList

--- AppMobile/Control/app/src/main/java/com/example/control/StringLinkedList.java
+++ AppMobile/Control/app/src/main/java/com/example/control/StringLinkedList.java
@@ -1,6 +1,78 @@
 package com.example.control;
 
 public class StringLinkedList {
+    private Node head;
+    private int size;
 
+    private class Node {
+        private String data;
+        private Node next;
 
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public StringLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(String data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
+    }
+
+    public String get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.data;
+    }
+
+    public String remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        String removedData;
+        if (index == 0) {
+            removedData = head.data;
+            head = head.next;
+        } else {
+            Node currentNode = head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            removedData = currentNode.next.data;
+            currentNode.next = currentNode.next.next;
+        }
+        size--;
+        return removedData;
+    }
+
+    public String remove() {
+        return remove(size - 1);
+    }
 }
