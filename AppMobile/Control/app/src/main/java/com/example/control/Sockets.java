de6896026d3b44efc1d96aa1a05611fc90003b0f
Pablo Sánchez <pablosanchez19@estudiantec.cr>
2023-05-11 01:28:03 -0600

    Configuracion de Intent

--- AppMobile/Control/app/src/main/java/com/example/control/Sockets.java
+++ AppMobile/Control/app/src/main/java/com/example/control/Sockets.java
@@ -2,13 +2,23 @@ package com.example.control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Sockets extends AppCompatActivity {
 
+    Button send_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sockets);
+
+        send_button = findViewById(R.id.send_button);
+
+        send_button.setOnClickListener(view -> {
+            Intent gotoactivitycontrol = new Intent(getApplicationContext(), Control.class);
+            startActivity(gotoactivitycontrol);
+        });
     }
 }
