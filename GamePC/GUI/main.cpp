95fad3ff0d98b4b429d1bc6b66a680c463f45557
Pablo Sánchez <pablosanchez19@estudiantec.cr>
2023-05-11 18:37:29 -0600

    Creacion del servidor GUI

---
+++ GamePC/GUI/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+void SocketServer() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Crear socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Error al crear socket" << std::endl;
+        return;
+    }
+
+    // Opción de socket para reutilizar la dirección
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        std::cerr << "Error en setsockopt" << std::endl;
+        return;
+    }
+
+    // Configurar dirección del socket
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(5001);
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cerr << "Error en setsockopt" << std::endl;
+        return;
+    }
+
+    // Enlazar socket a la dirección y puerto especificados
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        std::cerr << "Error en bind" << std::endl;
+        return;
+    }
+
+    // Escuchar conexiones entrantes
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Error en listen" << std::endl;
+        return;
+    }
+
+    std::cout << "Servidor en espera de conexiones..." << std::endl;
+
+    // Aceptar nueva conexión
+    if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
+        std::cerr << "Error en accept" << std::endl;
+        return;
+    }
+
+    std::cout << "Nueva conexión aceptada" << std::endl;
+
+    // Procesar mensajes entrantes
+    char buffer[1024] = {0};
+    int valread = read(new_socket, buffer, 1024);
+    if (valread <= 0) {
+        std::cout << "Cliente desconectado" << std::endl;
+        close(new_socket);
+        return;
+    }
+    std::cout << "Mensaje recibido: " << buffer << std::endl;
+
+    char respuesta[] = "Mensaje recibido.\n";
+    send(new_socket, respuesta, sizeof(respuesta), 0);
+    std::cout << "Respuesta: " << respuesta << std::endl;
+
+    if (close(server_fd) == -1) {
+        std::cerr << "Error al cerrar el socket: " << std::strerror(errno) << std::endl;
+        return;
+    }
+    if (close(new_socket) == -1) {
+        std::cerr << "Error al cerrar el socket: " << std::strerror(errno) << std::endl;
+        return;
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    while(true) { SocketServer(); }
+}
